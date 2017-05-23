1ff2b93ec640a2665f6d146adc2a521b12a4fa81
javacasm <javacasm@gmail.com>
2017-05-23 19:25:47 +0200

    v2

--- codigo/TestSpinner_v02.ino
+++ codigo/TestSpinner_v02.ino
@@ -9,21 +9,31 @@ void setup() {
   pinMode(PIN_LED,OUTPUT);
 }
 int old_valorSensor=0;
+int iContador=0;
+// Usamos dos potenciometros analogicos para ajustar el umbral mínimo y máximo
+int valorPotUmbralMax=analogRead(PIN_POTENCIOMETRO_UMBRAL_MAX);
+int valorPotUmbralMin=analogRead(PIN_POTENCIOMETRO_UMBRAL_MIN);
+
 void loop() {
   int valorSensor=analogRead(PIN_SENSOR_OPTICO);
   int diffSensor=valorSensor-old_valorSensor; // Calculamos la diferencia con el valor anterior
   old_valorSensor=valorSensor;
 
 
-// Usamos dos potenciometros analogicos para ajustar el umbral mínimo y máximo
-  int valorPotUmbralMax=analogRead(PIN_POTENCIOMETRO_UMBRAL_MAX);
-  int valorPotUmbralMin=analogRead(PIN_POTENCIOMETRO_UMBRAL_MIN);
-
-  Serial.println(String(valorPotUmbralMin)+
+  if(iContador%1000==0){
+    valorPotUmbralMax=analogRead(PIN_POTENCIOMETRO_UMBRAL_MAX);
+    valorPotUmbralMin=analogRead(PIN_POTENCIOMETRO_UMBRAL_MIN);
+  }
+  iContador  ++;
+  
+  /*Serial.println(String(valorPotUmbralMin)+
       ", "+String(valorSensor)+
       ", "+String(diffSensor)+
       ", "+String(valorPotUmbralMax));
-
+*/
+Serial.print(valorSensor);
+Serial.print(",");
+Serial.println(diffSensor);
   // activamos el led cuando el valor esté entre los umbrales
   if((valorSensor>valorPotUmbralMax) && (valorSensor<valorPotUmbralMin))
   { digitalWrite(PIN_LED,HIGH);}
