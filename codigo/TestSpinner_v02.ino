abe3e58b2571319c15e9e8b25075460be56cfe74
javacasm <javacasm@gmail.com>
2017-05-23 18:45:34 +0200

    v2

--- codigo/TestSpinner_v02.ino
+++ codigo/TestSpinner_v02.ino
@@ -1,17 +1,27 @@
 #define PIN_SENSOR_OPTICO A0
-#define PIN_POTENCIOMETRO_UMBRAL A1
+#define PIN_POTENCIOMETRO_UMBRAL_MAX A3
+#define PIN_POTENCIOMETRO_UMBRAL_MIN A2
+
+#define PIN_LED 13
 
 void setup() {
-Serial.begin(115200);
+  Serial.begin(115200);
+  pinMode(PIN_LED,OUTPUT);
 }
 
 void loop() {
   int valorSensor=analogRead(PIN_SENSOR_OPTICO);
 
-  Serial.println(valorSensor);
+  
 
 // Usamos un potenciometro analogico para ajustar el umbral
-  int valorPotUmbral=analogRead(PIN_POTENCIOMETRO_UMBRAL);
+  int valorPotUmbralMax=analogRead(PIN_POTENCIOMETRO_UMBRAL_MAX);
+  int valorPotUmbralMin=analogRead(PIN_POTENCIOMETRO_UMBRAL_MIN);
 
-  
+  Serial.println(String(valorPotUmbralMin)+", "+String(valorSensor)+", "+String(valorPotUmbralMax));
+
+  if((valorSensor>valorPotUmbralMax) && (valorSensor<valorPotUmbralMin))
+  { digitalWrite(PIN_LED,HIGH);}
+  else
+  { digitalWrite(PIN_LED,LOW);}
 }
